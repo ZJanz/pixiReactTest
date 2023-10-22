8ccf387d5b86e7e973e387c723599b9a27e4588f
ZJanz <ZACHATTACK100000@gmail.com>
2023-10-22 18:47:47 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { BlurFilter } from 'pixi.js';
 import { Stage, Container, Sprite, Text, useTick  } from '@pixi/react';
 import { useMemo } from 'react'
@@ -26,17 +26,80 @@ function Bunny(){
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [i, setI] = useState(0);
+  const [wKey, setWKey] = useState(false);
+  const [aKey, setAKey] = useState(false);
+  const [sKey, setSKey] = useState(false);
+  const [dKey, setDKey] = useState(false);
+
 
   const [rotation, setRotation] = useState(0);
 
   // custom ticker
   useTick(delta => {
     setI(i+ 0.05 * delta);
-    setX(x+1*delta);
-    setY(y+1*delta);
+    if(wKey === true){
+      setY(y-5*delta);
+    }
+    if(sKey === true){
+      setY(y+5*delta);
+    }
+    if(aKey === true){
+      setX(x-5*delta);
+    }
+    if(dKey === true){
+      setX(x+5*delta);
+    }
+    // setX(x+1*delta);
+    // setY(y+1*delta);
     setRotation(Math.sin(i) * Math.PI);
   });
+  useEffect(() => {
+    // Add event listeners for keyboard controls
+    const handleKeyDown = event => {
+      switch (event.key) {
+        case 'w':
+          setWKey(true)
+          break;
+        case 's':
+          setSKey(true)
+          break;
+        case 'a':
+          setAKey(true)
+          break;
+        case 'd':
+          setDKey(true)
+          break;
+        default:
+          break;
+      }
+    };
+      const handleKeyUp = event => {
+        switch (event.key) {
+          case 'w':
+            setWKey(false)
+            break;
+          case 's':
+            setSKey(false)
+            break;
+          case 'a':
+            setAKey(false)
+            break;
+          case 'd':
+            setDKey(false)
+            break;
+          default:
+            break;
+        }
+    };
+    window.addEventListener('keyup', handleKeyUp);
 
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [x, y]);
 
 
   return(<Sprite 
