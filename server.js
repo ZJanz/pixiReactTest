cb30c284dbde19c72f7e541af55377da63bb9b9a
ZJanz <ZACHATTACK100000@gmail.com>
2023-10-27 21:59:31 -0400

    Progress on controls and rotation
    Problem with anchoring

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
 let gameState = {
   t : Date.now(),
   playerArray: [{
+    rotation:0,
     x: 5,
     y: 5,
     velocityX:0,
@@ -31,12 +32,22 @@ gameState.playerIDToIndex.set(`${gameState.playerArray[0].id}`, 0)
 function update(){
   const lastTimestamp = gameState.t
   gameState.t = Date.now()
-  const delta = (gameState.t - lastTimestamp) /60;
+  const delta = (gameState.t - lastTimestamp) /16;
 
   for(i=0; i<gameState.playerArray.length; i++){
     let player = gameState.playerArray[i]
+    if(player.rotateLeft === true){
+      player.rotationVelocity = (player.rotationVelocity - 0.01*delta)
+    }
+    if(player.rotateRight === true){
+      player.rotationVelocity = (player.rotationVelocity + 0.01*delta)
+    }
     if (player.moveForward === true && player.moveDown === false && player.moveLeft === false && player.moveRight === false) {
-      player.velocityY = (player.velocityY - 1 * delta);
+      // player.velocityY = (player.velocityY - 1 * delta);
+
+      const angleInRadians = player.rotation; 
+      player.velocityX += Math.cos(angleInRadians) * 1*delta;
+      player.velocityY += Math.sin(angleInRadians) * 1*delta;
     }
     if (player.moveDown === true && player.moveForward === false && player.moveLeft === false && player.moveRight === false) {
       player.velocityY = (player.velocityY + 1 * delta);
@@ -64,6 +75,8 @@ function update(){
       player.velocityX = (player.velocityX + 0.5 * delta);
     }
 
+    player.rotation += (player.rotationVelocity * delta)
+
     player.x +=(player.velocityX * delta);
     player.y += (player.velocityY * delta);
     
@@ -85,6 +98,8 @@ io.on('connection', (socket) => {
     moveLeft: false,
     moveForward: false,
     moveDown: false,
+    rotationVelocity: 0,
+    rotation: 0,
 
     direction: 0,
     id: socket.id,
@@ -113,6 +128,12 @@ io.on('connection', (socket) => {
     if(data.key === 'd'){
       gameState.playerArray[playerArrayPosition].moveRight = true
     }
+    if(data.key === 'q'){
+      gameState.playerArray[playerArrayPosition].rotateLeft = true
+    }
+    if(data.key === 'e'){
+      gameState.playerArray[playerArrayPosition].rotateRight = true
+    }
     // You can perform any necessary server-side logic here based on the received key.
   });
 
@@ -132,6 +153,12 @@ io.on('connection', (socket) => {
     }
     if(data.key === 'd'){
       gameState.playerArray[playerArrayPosition].moveRight = false
+    }
+    if(data.key === 'q'){
+      gameState.playerArray[playerArrayPosition].rotateLeft = false
+    }
+    if(data.key === 'e'){
+      gameState.playerArray[playerArrayPosition].rotateRight = false
     }
     // You can perform any necessary server-side logic here based on the received key.
   });
